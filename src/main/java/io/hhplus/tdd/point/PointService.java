8e5bbc0a45e53f8fd2418e41a69fad9f821de420
gil <fkdl3919@gmail.com>
2024-12-17 19:21:22 +0900

    refactor: 포인트 충전 검증로직 리팩토링
    입력받은 amount에 대한 검증을 UserPoint 객체 내로 이동

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -19,16 +19,8 @@ public class PointService {
             throw new IllegalArgumentException("입력한 유저가 존재하지 않습니다.");
         }
 
-        // 입력한 point가 0 일 경우
-        if(amount == 0) {
-            throw new IllegalArgumentException("포인트가 입력되지 않았습니다.");
-        }
-
-        // 입력한 amount가 유효한 범위내의 값이 아닌 경우 ( 0 < point <= 1000 )
-        if(0 >= amount || amount > 1000) {
-            throw new IllegalArgumentException("포인트는 0 초과 1000 이하 범위 내로 입력해 주세요.");
-        }
-
+        // 입력받은 amount에 대해 검증
+        userPoint.validAmount(amount);
 
         return null;
     }
