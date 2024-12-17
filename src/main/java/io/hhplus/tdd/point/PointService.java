025b154e96c29733c9ae35b3618325cb69ef5651
gil <fkdl3919@gmail.com>
2024-12-17 20:30:07 +0900

    feat: 포인트를 사용한다. case 1 succ test 작성
    테스트 내용 case 1
    - 포인트 사용 시 입력한 id의 사용자가 존재하지 않는 경우

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -28,4 +28,17 @@ public class PointService {
         return userPointTable.insertOrUpdate(updatedUser.id(), updatedUser.point());
     }
 
+    public UserPoint use(long id, long amount){
+
+        UserPoint userPoint = userPointTable.selectById(id);
+
+        // 입력한 id의 유저가 존재하지 않을 시
+        if(userPoint == null) {
+            throw new IllegalArgumentException("입력한 유저가 존재하지 않습니다.");
+        }
+
+        return null;
+    }
+
+
 }
